e329bb999a96449c774dfb64fdbe20c989642bc6
Dilshad <dilshad@ee.iitr.ac.in>
2021-09-28 10:56:11 +0530

    Square Root of a Number

---
+++ Binary Search/Square_root_of_a_Number.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+	
+     long start=1,end=x,ans =1;
+     
+     while(start<=end)
+     {
+         long mid= start + (end - start)/2;
+         if(mid<=x/mid)
+         {
+             ans=mid;
+             start = mid+1;
+         }
+         else
+         end=mid-1;
+     }
+     return ans;
+	 }
+}
