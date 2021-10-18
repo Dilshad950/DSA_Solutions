ddca27ccbbe65c4ab1b3dc75b4f4c8238d79d624
Dilshad <dilshad@ee.iitr.ac.in>
2021-10-18 12:47:37 +0530

    Segregate nodes with even and odd values

---
+++ Linked_List/Segregate_Even_Odd.java
@@ -0,0 +1,127 @@
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+	static void printList(Node node, PrintWriter out) 
+	{ 
+		while (node != null) 
+		{ 
+			out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node ans = ob.divide(n, head);
+            printList(ans, out); 
+            t--;
+        }
+        out.flush();
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+
+/* 
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution{
+    Node divide(int N, Node head){
+        // code here
+        Node temp = head ;
+        Node temp1 = new Node(0);
+        Node ans1 = temp1;
+        Node temp2 = new Node(0);
+        Node ans2 = temp2;
+        
+        while(temp!=null)
+        {
+            if(temp.data%2==0)
+            {
+                Node add = new Node(temp.data);
+                temp1.next = add;
+                temp1 = temp1.next;
+            }
+            else
+            {
+                Node add = new Node(temp.data);
+                temp2.next = add;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        
+        Node temp3 = ans1.next;
+        if(temp3==null)
+        {
+            return head;
+        }
+        
+        while(temp3.next!=null)
+        {
+            temp3 = temp3.next;
+        }
+        temp3.next = ans2.next;
+        return ans1.next;
+    }
+}
